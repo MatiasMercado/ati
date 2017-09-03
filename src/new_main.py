42a92979d4f5c689d340618f77cc11c4bf2fb6bb
mati <matimercado06@hotmail.com>
2017-09-03 01:08:56 -0300

    Migrated All Menus - ERR on p_median_filter

--- src/new_main.py
+++ src/new_main.py
@@ -4,6 +4,7 @@ import PIL
 from PIL import ImageTk
 import numpy as np
 
+from input.filter_provider import FilterProvider
 from input.provider import Provider
 from input.util import Util
 from matplotlib import pyplot as plt
@@ -22,6 +23,7 @@ class ImageEditor(tk.Frame):
         # File = askopenfilename(parent=root, initialdir="C:/",title='Choose an image.')
 
         # Parameters
+
         # Contrast
         self.s1 = 70
         self.s2 = 150
@@ -35,17 +37,33 @@ class ImageEditor(tk.Frame):
         # Scalar Product
         self.scalar = 1.2
 
+        # Noise
+        self.normal_mu = 0
+        self.normal_sigma = 1
+        self.normal_prob = 0.5
+        self.rayleigh_scale = 1
+        self.exp_scale = 1
+        self.exp_prob = 0.5
+        self.salt_pepper_prob = (0.25, 0.25)
+
+        # Filters
+        self.mean_filter_size = (3, 3)
+        self.median_filter_mask = np.matrix([[1, 3, 1], [3, 5, 3], [1, 3, 1]])
+        self.p_median_filter_mask = np.matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+        self.normal_filter_size = (3, 3)
+        self.normal_filter_sigma = 0.5
+
     def create_menu(self):
         root = self.master
 
         # Menu Bar
         menu_bar = tk.Menu(root)
         # file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu = tk.Menu(menu_bar)
-        transform_menu = tk.Menu(menu_bar)
-        noise_menu = tk.Menu(menu_bar)
-        filter_menu = tk.Menu(menu_bar)
-        operations_menu = tk.Menu(menu_bar)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        transform_menu = tk.Menu(menu_bar, tearoff=0)
+        noise_menu = tk.Menu(menu_bar, tearoff=0)
+        filter_menu = tk.Menu(menu_bar, tearoff=0)
+        operations_menu = tk.Menu(menu_bar, tearoff=0)
 
         # File Menu
         file_menu.add_command(label='Load', command=self.load_image)
@@ -53,7 +71,7 @@ class ImageEditor(tk.Frame):
         file_menu.add_command(label='Save')
         file_menu.add_separator()
         file_menu.add_command(label='Save selection')
-        file_menu.add_command(label='Histogram')
+        file_menu.add_command(label='Histogram', command=self.histogram)
         file_menu.add_separator()
         file_menu.add_command(label='Quit', command=root.destroy)
         menu_bar.add_cascade(label='File', menu=file_menu)
@@ -75,22 +93,24 @@ class ImageEditor(tk.Frame):
         menu_bar.add_cascade(label='Operations', menu=operations_menu)
 
         # Noise Menu
-        noise_menu.add_command(label='Normal')
-        noise_menu.add_command(label='Rayleigh')
-        noise_menu.add_command(label='Exp')
-        noise_menu.add_command(label='Salt-pepper')
+        noise_menu.add_command(label='Normal', command=self.normal_noise)
+        noise_menu.add_command(label='Rayleigh', command=self.rayleigh_noise)
+        noise_menu.add_command(label='Exp', command=self.exp_noise)
+        noise_menu.add_command(label='Salt-pepper', command=self.salt_pepper_noise)
         menu_bar.add_cascade(label='Noise', menu=noise_menu)
 
         # Filter Menu
-        filter_menu.add_command(label='Mean')
-        filter_menu.add_command(label='Median')
-        filter_menu.add_command(label='P. Median')
-        filter_menu.add_command(label='Normal')
-        filter_menu.add_command(label='Borders')
+        filter_menu.add_command(label='Mean', command=self.mean_filter)
+        filter_menu.add_command(label='Median', command=self.median_filter)
+        filter_menu.add_command(label='P. Median', command=self.p_median_filter)
+        filter_menu.add_command(label='Normal', command=self.normal_filter)
+        filter_menu.add_command(label='Borders', command=self.borders_filter)
         menu_bar.add_cascade(label='Filter', menu=filter_menu)
 
         # Add menu Bar to Root Window
         root.config(menu=menu_bar)
+
+    # File Menu Functions
 
     def load_image(self):
         (width, height) = self.size
@@ -117,7 +137,7 @@ class ImageEditor(tk.Frame):
         new_window.bind('<Destroy>', self.remove_open_image)
 
         self.image_number += 1
-        canvas = tk.Canvas(new_window, width=width, height=height)
+        canvas = tk.Canvas(new_window, width=width, height=height, borderwidth=0, highlightthickness=0)
         canvas.grid(row=0, column=0)
         # canvas.create_image(-.5, -.5, image=tk_img, anchor=tk.NW)
         canvas.create_image(0, 0, image=tk_img, anchor=tk.NW)
@@ -130,7 +150,26 @@ class ImageEditor(tk.Frame):
     def remove_open_image(self, event):
         self.open_images.pop(event.widget.winfo_toplevel().title(), None)
 
-    # Transform Functions
+    def histogram(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        plt.hist(image.flatten(), bins=range(256))
+        plt.show()
+
+    def edit_image(self):
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        img_data = self.load_image()
+        pil_img = PIL.Image.fromarray(img_data, 'RGB')
+        tk_img = ImageTk.PhotoImage(pil_img)
+        self.label = tk.Label(self.master, image=self.tk_img).grid(row=0, column=0)
+        self.label.my_image = tk_img  # Used only to prevent image being destroy by garbage collector
+
+    # Transform Menu Functions
 
     def negative(self):
         self.wait_variable(self.active_window)
@@ -144,7 +183,7 @@ class ImageEditor(tk.Frame):
         r = Util.contrast_increase(image[:, :, 0], self.s1, self.s2)
         g = Util.contrast_increase(image[:, :, 1], self.s1, self.s2)
         b = Util.contrast_increase(image[:, :, 2], self.s1, self.s2)
-        transformed_img = self.merge_rgb(r, g, b)
+        transformed_img = self.__merge_rgb(r, g, b)
         self.create_new_image(transformed_img)
 
     def dynamic_compression(self):
@@ -153,7 +192,7 @@ class ImageEditor(tk.Frame):
         r = Util.dynamic_range_compression(image[:, :, 0])
         g = Util.dynamic_range_compression(image[:, :, 1])
         b = Util.dynamic_range_compression(image[:, :, 2])
-        transformed_img = self.merge_rgb(r, g, b)
+        transformed_img = self.__merge_rgb(r, g, b)
         self.create_new_image(transformed_img)
 
     def gamma_function(self):
@@ -206,7 +245,68 @@ class ImageEditor(tk.Frame):
         transformed_img = Util.scalar_prod(image, self.scalar)
         self.create_new_image(transformed_img)
 
-    def merge_rgb(self, r, g, b):
+    # Noise Functions
+
+    def normal_noise(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = Util.add_additive_noise_normal(image, self.normal_mu, self.normal_sigma,
+                                                         self.normal_prob)
+        self.create_new_image(transformed_img)
+
+    def rayleigh_noise(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = Util.add_noise_rayleigh(image, self.rayleigh_scale)
+        self.create_new_image(transformed_img)
+
+    def exp_noise(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = Util.add_noise_exponential(image, self.exp_scale, self.exp_prob)
+        self.create_new_image(transformed_img)
+
+    def salt_pepper_noise(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = Util.add_comino_and_sugar_noise(image, self.salt_pepper_prob)
+        self.create_new_image(transformed_img)
+
+    # Filter Functions
+
+    def mean_filter(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = FilterProvider.blur(image, self.mean_filter_size)
+        self.create_new_image(transformed_img)
+
+    def median_filter(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = FilterProvider.median_filter(image, self.median_filter_mask, False)
+        self.create_new_image(transformed_img)
+
+    def p_median_filter(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = FilterProvider.sliding_window_median(image, self.p_median_filter_mask, True)
+        self.create_new_image(transformed_img)
+
+    def normal_filter(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = FilterProvider.gauss_blur(image, self.normal_filter_size, self.normal_filter_sigma)
+        self.create_new_image(transformed_img)
+
+    def borders_filter(self):
+        self.wait_variable(self.active_window)
+        image = self.open_images[self.active_window.get()]
+        transformed_img = FilterProvider.pasa_altos(image)
+        self.create_new_image(transformed_img)
+
+    # Private Functions
+
+    def __merge_rgb(self, r, g, b):
         ans = np.zeros((r.shape[0], r.shape[1], 3))
         for i in range(r.shape[0]):
             for j in range(r.shape[1]):
@@ -214,19 +314,6 @@ class ImageEditor(tk.Frame):
                 ans[i][j][1] = g[i][j]
                 ans[i][j][2] = b[i][j]
         return ans
-
-    def edit_image(self):
-        top = self.winfo_toplevel()
-        top.rowconfigure(0, weight=1)
-        top.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
-
-        img_data = self.load_image()
-        pil_img = PIL.Image.fromarray(img_data, 'RGB')
-        tk_img = ImageTk.PhotoImage(pil_img)
-        self.label = tk.Label(self.master, image=self.tk_img).grid(row=0, column=0)
-        self.label.my_image = tk_img  # Used only to prevent image being destroy by garbage collector
 
 if __name__ == '__main__':
     app = ImageEditor()
