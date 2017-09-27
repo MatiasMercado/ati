28a09240ea4b56f8280abfbe9608b06ef60127f2
mati <matimercado06@hotmail.com>
2017-09-26 23:17:10 -0300

    Edit shows static input

--- src/new_main.py
+++ src/new_main.py
@@ -25,13 +25,21 @@ class ImageEditor(tk.Frame):
 
         # Create Widgets
         self.init_default_settings()
-        self.settings = self.create_settings()
+        self.settings = self.create_settings_window()
         self.create_menu()
 
         # State Variables
         self.active_window = tk.StringVar()
         self.image_number = 1
         self.open_images = {}
+
+        self.x_coord = tk.DoubleVar();
+        self.y_coord = tk.DoubleVar();
+        self.r_value = tk.DoubleVar();
+        self.g_value = tk.DoubleVar();
+        self.b_value = tk.DoubleVar();
+        # is_selected
+        # selection_x1, y1, x2, y2
 
     def create_menu(self):
         root = self.master
@@ -46,7 +54,7 @@ class ImageEditor(tk.Frame):
 
         # File Menu
         file_menu.add_command(label='Load', command=self.load_image)
-        file_menu.add_command(label='Edit')
+        file_menu.add_command(label='Edit', command=self.edit_image)
         file_menu.add_command(label='Save', command=self.save_image)
         file_menu.add_separator()
         file_menu.add_command(label='Save selection')
@@ -141,7 +149,7 @@ class ImageEditor(tk.Frame):
         self.gauss_filter_sigma = tk.DoubleVar()
         self.gauss_filter_sigma.set(20)
 
-    def create_settings(self):
+    def create_settings_window(self):
         settings_frame = tk.Frame(self)
         self.settings_row = 0;
 
@@ -287,45 +295,83 @@ class ImageEditor(tk.Frame):
         plt.hist(image.flatten(), bins='auto', normed=1)
         plt.show()
 
-    def create_edit_panel(self):
-        edit_frame = tk.Frame(self, bd=1)
-        # self.edited_image_label = tk.Label(edit_frame).grid()
-        tk.Button(edit_frame, text='Discard', command=lambda: self.hide_edit_panel)\
-            .grid(columnspan=2, sticky=(tk.W, tk.E))
-        tk.Button(edit_frame, text='Finish', command=lambda: self.create_new_image(self.edited_image))\
-            .grid(columnspan=2, sticky=(tk.W, tk.E))
-        return edit_frame
-        # ----------------- DELETE ----------------------------
-        # top = self.winfo_toplevel()
-        # top.rowconfigure(0, weight=1)
-        # top.columnconfigure(0, weight=1)
-        # self.rowconfigure(0, weight=1)
-        # self.columnconfigure(0, weight=1)
-        # img_data = self.load_image()
-        # pil_img = PIL.Image.fromarray(img_data, 'RGB')
-        # tk_img = ImageTk.PhotoImage(pil_img)
-        # self.label = tk.Label(self.master, image=self.tk_img).grid(row=0, column=0)
-        # self.label.my_image = tk_img  # Used only to prevent image being destroy by garbage collector
-        # -----------------------------------------------
-
-    def show_edit_panel(self):
-        edit_frame = tk.Frame(self, bd=1)
+    def edit_image(self):
         self.wait_variable(self.active_window)
-        image = self.open_images[self.active_window.get()]
-        self.edited_image = np.copy(image).astype(float)
-        linear_image = Util.linear_transform(self.edited_image)
-        tk.Label(edit_frame, image=linear_image).grid()
-        # edited_image_label.config(image = self.edited_image)
-        self.edit_panel = edit_frame
-        self.edit_panel.grid()
-        tk.Button(edit_frame, text='Discard', command=lambda: self.hide_edit_panel) \
-            .grid(columnspan=2, sticky=(tk.W, tk.E))
-        tk.Button(edit_frame, text='Finish', command=lambda: self.create_new_image(self.edited_image)) \
-            .grid(columnspan=2, sticky=(tk.W, tk.E))
-        return edit_frame
+        img_data = self.open_images[self.active_window.get()]
 
-    def hide_edit_panel(self):
-        self.edit_panel.grid_remove()
+        linear_img = Util.linear_transform(img_data)
+        pil_img = PIL.Image.fromarray(linear_img, 'RGB')
+        tk_img = ImageTk.PhotoImage(pil_img)
+        height = img_data.shape[0]
+        width = img_data.shape[1]
+
+        root = self.master;
+        canvas = tk.Canvas(root, width=width, height=height, borderwidth=0, highlightthickness=0)
+        canvas.grid(row=0, column=1)
+        canvas.create_image(0, 0, image=tk_img, anchor=tk.NW)
+        canvas.my_image = tk_img  # Used only to prevent image being destroy by garbage collector
+
+        self.edited_img_data = np.copy(img_data).astype(float)
+        # self.edited_img_canvas = canvas
+
+        coord_frame = tk.Frame(self)
+        tk.Label(coord_frame, text='(x,y)').grid(row=0, column=0, columnspan=3)
+        tk.Entry(coord_frame, text=self.x_coord, textvariable=self.x_coord, width=5).grid(row=1,column=0)
+        tk.Entry(coord_frame, text=self.y_coord, textvariable=self.y_coord, width=5).grid(row=1,column=1)
+        tk.Label(coord_frame, text='(r,g,b)').grid(row=2, column=0, columnspan=3)
+        tk.Entry(coord_frame, text=self.r_value, textvariable=self.r_value, width=5).grid(row=3, column=0)
+        tk.Entry(coord_frame, text=self.g_value, textvariable=self.g_value, width=5).grid(row=3, column=1)
+        tk.Entry(coord_frame, text=self.b_value, textvariable=self.b_value, width=5).grid(row=3, column=2)
+        tk.Button(coord_frame, text='Set Value').grid(row = 1, column = 2, padx=10)
+
+        ttk.Separator(coord_frame, orient=tk.HORIZONTAL).grid(row=5, columnspan=3, sticky=(tk.W, tk.E), pady=10)
+
+        tk.Button(coord_frame, text='Cancel').grid(row=6, column=0, columnspan=1, pady=10)
+        tk.Button(coord_frame, text='Save').grid(row=6, column=2, columnspan=1, pady=10)
+
+        # TODO: return coord_frame and save it to hide it later (copy create_settings method)
+        coord_frame.grid(row=0, column=0)
+
+
+    # def create_edit_panel(self):
+    #     edit_frame = tk.Frame(self, bd=1)
+    #     # self.edited_image_label = tk.Label(edit_frame).grid()
+    #     tk.Button(edit_frame, text='Discard', command=lambda: self.hide_edit_panel)\
+    #         .grid(columnspan=2, sticky=(tk.W, tk.E))
+    #     tk.Button(edit_frame, text='Finish', command=lambda: self.create_new_image(self.edited_image))\
+    #         .grid(columnspan=2, sticky=(tk.W, tk.E))
+    #     return edit_frame
+    #     # ----------------- DELETE ----------------------------
+    #     # top = self.winfo_toplevel()
+    #     # top.rowconfigure(0, weight=1)
+    #     # top.columnconfigure(0, weight=1)
+    #     # self.rowconfigure(0, weight=1)
+    #     # self.columnconfigure(0, weight=1)
+    #     # img_data = self.load_image()
+    #     # pil_img = PIL.Image.fromarray(img_data, 'RGB')
+    #     # tk_img = ImageTk.PhotoImage(pil_img)
+    #     # self.label = tk.Label(self.master, image=self.tk_img).grid(row=0, column=0)
+    #     # self.label.my_image = tk_img  # Used only to prevent image being destroy by garbage collector
+    #     # -----------------------------------------------
+    #
+    # def show_edit_panel(self):
+    #     edit_frame = tk.Frame(self, bd=1)
+    #     self.wait_variable(self.active_window)
+    #     image = self.open_images[self.active_window.get()]
+    #     self.edited_image = np.copy(image).astype(float)
+    #     linear_image = Util.linear_transform(self.edited_image)
+    #     tk.Label(edit_frame, image=linear_image).grid()
+    #     # edited_image_label.config(image = self.edited_image)
+    #     self.edit_panel = edit_frame
+    #     self.edit_panel.grid()
+    #     tk.Button(edit_frame, text='Discard', command=lambda: self.hide_edit_panel) \
+    #         .grid(columnspan=2, sticky=(tk.W, tk.E))
+    #     tk.Button(edit_frame, text='Finish', command=lambda: self.create_new_image(self.edited_image)) \
+    #         .grid(columnspan=2, sticky=(tk.W, tk.E))
+    #     return edit_frame
+    #
+    # def hide_edit_panel(self):
+    #     self.edit_panel.grid_remove()
 
     # Transform Menu Functions
     def negative(self):
@@ -473,5 +519,5 @@ class ImageEditor(tk.Frame):
 if __name__ == '__main__':
     app = ImageEditor()
     app.master.title('Image Editor')
-    app.master.geometry('400x530')
+    app.master.geometry('500x530')
     app.mainloop()
