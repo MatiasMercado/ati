4daf19fe83f53ac0fade4f7dc861dc00a5302eea
jcl <jlepore@itba.edu.ar>
2017-10-03 00:33:10 -0300

    laplacian gaussian

--- src/input/border_detectors.py
+++ src/input/border_detectors.py
@@ -17,6 +17,28 @@ class BorderDetector:
         return aux_image
 
     @staticmethod
+    def laplacian_gaussian_detector(image, sigma):
+
+        size = 7, 7
+        mask = np.zeros(size)
+
+        def gauss_function(x, y):
+            return np.exp(-(x ** 2 + y ** 2) / (2 * (sigma ** 2))) * (
+                2 - ((x ** 2 + y ** 2) / (sigma ** 2))) / (-np.sqrt(2 * np.pi) * sigma ** 3)
+
+        # Create Mask
+        for i in range(size[0]):
+            for j in range(size[1]):
+                mask_index_i = i - int(mask.shape[0] / 2)  # Go from -rows/2 to +rows/2
+                mask_index_j = j - int(mask.shape[1] / 2)  # Go from -cols/2 to +cols/2
+                mask[i][j] = gauss_function(mask_index_i, mask_index_j)
+
+        aux_image = FilterProvider.sliding_window(image=image, mask=mask)
+        aux_image = BorderDetector.__zero_detector(aux_image, threshold=4)
+
+        return aux_image
+
+    @staticmethod
     def __zero_detector(image, independent_layer=False, threshold=DEFAULT_ZERO_DETECTOR_THRESHOLD):
         ans = np.zeros(image.shape)
         (image_width, image_height) = image.shape[0], image.shape[1]
@@ -27,16 +49,24 @@ class BorderDetector:
             return 0
 
         for x in range(image_width):
-            for y in range(image_height - 1):
+            for y in range(image_height):
                 if independent_layer:
                     for z in range(image.shape[2]):
-                        ans[x, y, z] = single_zero_detector(image[x, y, z], image[x, y + 1, z])
+                        if x != image_width - 1:
+                            ans[x, y, z] = single_zero_detector(image[x, y, z], image[x + 1, y, z])
+                        if ans[x, y, z] != 255 and y != image_height - 1:
+                            ans[x, y, z] = single_zero_detector(image[x, y, z], image[x, y + 1, z])
                 else:
-                    aux = single_zero_detector(image[x, y, 0], image[x, y + 1, 0])
+                    aux = 0
+                    if x != image_width - 1:
+                        aux = single_zero_detector(image[x, y, 0], image[x + 1, y, 0])
+                    if aux == 0 and y != image_height - 1:
+                        aux = single_zero_detector(image[x, y, 0], image[x, y + 1, 0])
                     for z in range(image.shape[2]):
                         ans[x, y, z] = aux
         return ans
 
+
 # img = Util.load_raw('LENA.RAW')
-# img = BorderDetector.laplacian_detector(img)
-# Util.save_raw(img, 'lena_laplacian_border')
+# img = BorderDetector.laplacian_gaussian_detector(img, 0.7)
+# Util.save_raw(img, 'lena_laplacian_gaussian_border')
