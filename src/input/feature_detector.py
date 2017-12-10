bf3edbb74fd4940ce3848c55c3a8423d899b30d5
mati <matimercado06@hotmail.com>
2017-12-10 16:16:47 -0300

    Curv and Cont energy functions

--- src/input/feature_detector.py
+++ src/input/feature_detector.py
@@ -1,3 +1,4 @@
+import numpy as np
 from cv2 import cv2
 
 
@@ -12,6 +13,31 @@ class FeaturesDetector:
         img = cv2.drawKeypoints(gray, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
         return img
+
+    @staticmethod
+    def curvature_energy(control_point, prev_point, next_point):
+        x, y = control_point
+        p1, p2 = prev_point
+        n1, n2 = next_point
+        return (p1 - 2*x + n1)**2 + (p2 -2*y + n2)**2
+
+    @staticmethod
+    def continuity_energy(control_point, average_distance, prev_point):
+        x, y = control_point
+        p1, p2 = prev_point
+        return average_distance - np.sqrt((x-p1)**2 + (y-p2)**2)
+
+    @staticmethod
+    def average_distance(control_points):
+        # Set last point as previous for the first element
+        prev = control_points[len(control_points)-1]
+        average = 0
+        for point in control_points:
+            x, y = point
+            p1, p2 = prev
+            average += np.sqrt((x-p1)**2 + (y-p2)**2)
+            prev = point
+        return average / len(control_points)
 
 #
 # image = Util.load_image('milo.JPG')
