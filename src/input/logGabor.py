de5e79cc74c0487e26a0539869e579b91a6bb26b
josigtz <josivina@gmail.com>
2017-12-15 13:22:08 -0600

    Normalization working

--- src/input/logGabor.py
+++ src/input/logGabor.py
@@ -1,34 +1,35 @@
 import numpy as np
 import cv2
 import math
-from LogGabor import imread
+import matplotlib.pyplot as plt
+from src.input.util import Util
+from src.input.provider import Provider
 
 def normalization(image, inner, outter):
-    iris=np.zeros((360,64))
-
+    iris=np.zeros((36,360,3))
     innerimax = max(inner[:][:])
     innerimin = min(inner[:][:])
-    center = (int((innerimax[0]-innerimin[0])/2), int(innerimin[1]+(innerimax[1]-innerimin[1])/2))
-    print(center)
-
-    innerradius = max(inner[:][:])[0] - center[0]
-    outterradius = max(outter[:][:])[0] - center[0]
-    print (innerradius)
-    print (outterradius)
-
+    center = (int((innerimax[1]+innerimin[1])/2), int((innerimax[0]+innerimin[0])/2))
+    innerradius = int(math.sqrt((center[0]-inner[0][1])**2+(center[1]-inner[0][0])**2))
+    outterradius = int(math.sqrt((center[0]-outter[0][1])**2+(center[1]-outter[0][0])**2))
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            pointradius = math.sqrt((center[0]-i)**2+(center[1]-j)**2)
-            if pointradius > innerradius and pointradius < outterradius:
-                theta = int(math.degrees(math.atan2(j - center[0], i - center[1])))
-                iris[theta][pointradius]=image[i][j]
-    return iris
+           for k in range(image.shape[2]):
+             pointradius = int(math.sqrt((center[0]-i)**2+(center[1]-j)**2))
+             if (pointradius > innerradius) and (pointradius < outterradius):
+               theta = int(math.degrees(math.atan2(j - center[1], i - center[0])))
+               if theta < 0:
+                 theta += 360
+               iris[pointradius-innerradius][theta][k]=image[i][j][k]
+               image[i][j][0]=0
+    return iris,image
 
 def interest_degrees(image):
-    ans = np.zeros(110,64) #Resize al tamaño de interes donde hay menos ruido
+    ans = np.zeros((110,34,3)) #Resize al tamaño de interes donde hay menos ruido
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            if i > 180 and i < 246 or i > 314 and i < 359:
+           for k in range(image.shape[2]):
+              if i > 180 and i < 246 or i > 314 and i < 359:
                 ans[i][j]=image[i][j]
     return ans
 
@@ -70,8 +71,4 @@ def compare_templates(basetemplate, template):
                acumm += 1
     hammingdist = acumm / total
     return hammingdist
-
-image=imread('lena.jpg')
-ceropercent(process(image,build_filters()))
-
 
